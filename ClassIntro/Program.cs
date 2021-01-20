bb89b75a7c347bdd153bb90754dedd7bbd6bae95
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-01-21 02:03:25 +0300

    16 Ocak

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Akif";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Mehmet Akif TURAN";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Pınar TURAN";
+            kurs2.IzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Irmak Nisa TURAN";
+            kurs3.IzlenmeOrani = 98;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Ali Kağan TURAN";
+            kurs4.IzlenmeOrani = 98;
+
+
+            //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (Kurs kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+            }
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır Karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------------");
+            }
+
+            Console.WriteLine("--------------Metotlar--------------");
+
+            //instance - örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 8, 9);
+            sepetManager.Ekle2("Karpuz", "İri Karpuz", 22, 8);
+
+
+        }
+    }
+}
+
+
+//Don't repeat yourself - DRY - Clean Code - Best Practice 
