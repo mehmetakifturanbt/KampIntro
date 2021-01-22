c88dcd0e748a4d174ae15224407f34d9cf88f724
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-01-23 02:58:17 +0300

    20 Ocak Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Ali");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Irmak");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+
+            //List<string> liste = new List<string>();
+            //Console.WriteLine(liste.Count);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
