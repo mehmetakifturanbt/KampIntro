7bfcb9806f34c710fcf76a088b577f683a2525c4
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-01-22 22:54:22 +0300

    OOP Eklendi

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //CRUD Operasyonları - Create Read Update Delete
+        //encapsulation
+
+        public void Add(Product product) 
+        {
+            //product.ProductName = "Kamera";
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi .");
+        }
+
+
+
+        //public int Topla(int sayi1, int sayi2)
+        //{
+        //    return sayi1 + sayi2;
+        //}
+
+        //public void Topla2(int sayi1, int sayi2)
+        //{
+        //    Console.WriteLine(sayi1 + sayi2); 
+        //}
+
+        //public void BiseyYap(int sayi)
+        //{
+        //    sayi = 99;
+        //}
+    }
+}
